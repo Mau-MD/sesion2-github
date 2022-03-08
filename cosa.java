e66cb8c4c867dfabf165bffdd652c3fb18d5f6cb
Clanie1 <danybarociov@gmail.com>
2022-03-08 13:30:22 -0800

    Create cosa.java

---
+++ cosa.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class cosa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String hola = scanner.nextLine();
+        System.out.println(hola);
+    }
+
+}
